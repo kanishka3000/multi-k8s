96a4d950fc29049a6d5c501cde00a844f1baf8be
KanishkaW <kkgweerasekara@gmail.com>
2020-04-26 15:40:01 +0530

    added scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t kanishka3000/multi-client:latest -t kanishka3000/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t kanishka3000/multi-server:latest -t kanishka3000/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t kanishka3000/multi-worker:latest -t kanishka3000/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push kanishka3000/multi-client:latest
+docker push kanishka3000/multi-client:$SHA
+docker push kanishka3000/multi-server:latest
+docker push kanishka3000/multi-server:$SHA
+docker push kanishka3000/multi-worker:latest
+docker push kanishka3000/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
